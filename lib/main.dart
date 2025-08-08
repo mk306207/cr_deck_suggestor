18e0bbc09dcfc6a08f302e4a1bde4d11a68d7dc5
Mateusz <kolbermateusz@gmail.com>
2025-08-08 18:32:12 +0200

    home  page

--- lib/main.dart
+++ lib/main.dart
@@ -4,24 +4,22 @@ void main() {
   runApp(const MyApp());
 }
 
-// Główna aplikacja Fluttera
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Formularz',
+      title: 'Test',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(seedColor: const Color.fromARGB(255, 160, 134, 205)),
         useMaterial3: true,
       ),
-      home: const MyFormPage(), // <-- formularz jako osobny ekran
+      home: const MyFormPage(),
     );
   }
 }
 
-// Ekran z formularzem
 class MyFormPage extends StatefulWidget {
   const MyFormPage({super.key});
 
@@ -40,46 +38,106 @@ class _MyFormPageState extends State<MyFormPage> {
   }
 
   void _submit() async{
-  final response = await http.get(Uri.parse('https://jsonplaceholder.typicode.com/posts/1'));
+    final response = await http.get(Uri.parse('https://crproxy-production-6a8c.up.railway.app/getDecks'));
 
-  if (response.statusCode == 200) {
-    print('Dane: ${response.body}');
-  } else {
-    print('Błąd: ${response.statusCode}');
+    if (response.statusCode == 200) {
+      print('Data: ${response.body}');
+    } else {
+      print('Error: ${response.statusCode}');
+    }
   }
-}
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Formularz')),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Form(
-          key: _formKey,
-          child: Column(
-            children: <Widget>[
-              TextFormField(
-                controller: _playerTag,
-                decoration: const InputDecoration(
-                  hintText: 'Insert your plater tag',
+      backgroundColor: Colors.grey[100],
+      body: Column(
+        children: [
+          Container(
+            padding: EdgeInsets.symmetric(vertical: 25, horizontal: 25),
+            height: 220,
+            width: double.infinity,
+            decoration: BoxDecoration(color: Colors.teal),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Text(
+                  "DECK SUGGESTOR FOR CLASH ROYALE",
+                  style: TextStyle(
+                    fontSize: 26,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                  ),
                 ),
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Please insert valid player tag';
-                  }
-                  return null;
-                },
-              ),
-              const SizedBox(height: 20),
-              ElevatedButton(
-                onPressed: _submit,
-                child: const Text('Wyślij'),
-              ),
-            ],
+                SizedBox(height: 8),
+                Text(
+                  "Mateusz Kolber",
+                  style: TextStyle(
+                    fontSize: 16,
+                    color: Colors.white70,
+                  ),
+                ),
+
+              ],
+            ),
           ),
-        ),
+          Padding(
+            padding: EdgeInsets.symmetric(vertical:150,horizontal: 15),
+            child: Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(15),
+                color: Colors.black12,
+              ),
+              padding: EdgeInsets.all(16),
+              child: Form(
+                key: _formKey,
+                child: Column(
+                  children: <Widget>[
+                    TextFormField(
+                      controller: _playerTag,
+                      decoration: InputDecoration(
+                        hintText: 'Insert your player tag',
+                          enabledBorder: UnderlineInputBorder(
+                            borderSide: BorderSide(color: Colors.black12),
+                          ),
+                          focusedBorder:  UnderlineInputBorder(
+                            borderSide: BorderSide(color: Colors.teal)
+                          )
+                      ),
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Please insert a valid player tag';
+                        }
+                        return null;
+                      },
+                    ),
+                    const SizedBox(height: 20),
+                    ElevatedButton(
+                      onPressed: _submit,
+                      style:ButtonStyle(
+                        overlayColor: getColor(Colors.white,Colors.teal),
+                        foregroundColor: getColor(Colors.teal,Colors.white)
+                      ),
+                      child: const Text('Search',),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
+  WidgetStateProperty<Color> getColor(Color color, Color colorPressed) {
+    return WidgetStateProperty.resolveWith((Set<WidgetState> states) {
+      if (states.contains(WidgetState.pressed)) {
+        return colorPressed;
+      } else {
+        return color;
+      }
+    });
+  }
+
 }
